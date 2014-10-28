9138daeaf2f42910586789e05271b9697a71673e
lorduriel <urieluvd@gmail.com>
2014-10-28 17:56:29 -0600

    Listo el post de recepcion de compra

---
+++ app/src/main/java/com/empresa/oscar/exportando/PostPurchaseReception.java
@@ -0,0 +1,137 @@
+package com.empresa.oscar.exportando;
+
+/**
+ * Created by UsuarioRasa on 28/10/2014.
+ */
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class PostPurchaseReception extends AsyncTask<Void, Void, String> {
+    private ProgressDialog progressDialog;
+    private HttpClient httpClient;
+    private HttpPost httpPost;
+    private List<NameValuePair> nameValuePairs;
+    private ResponseHandler<String> responseHandler;
+    private Activity context;
+    boolean error;
+    private String success,status;
+    private int purchase_id;
+    int id;
+    String nick,pass,type,authorization;
+
+
+    PostPurchaseReception(Activity context,int purchase_id ) {
+        this.context = context;
+        this.purchase_id=purchase_id;
+
+    }
+
+    protected void onPreExecute() {
+        // TODO Auto-generated method stub
+        super.onPreExecute();
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Registrando entrada, espere por favor...");
+        progressDialog.setIndeterminate(true);
+        progressDialog.show();
+    }
+
+    @Override
+    protected String doInBackground(Void... arg0) {
+        String response;
+        String aux;
+        JSONObject jsonObject;
+
+        SharedPreferences prefs = context.getSharedPreferences("Exporta",Activity.MODE_PRIVATE);
+        String usr=prefs.getString("Empleado",null);
+        String pass=prefs.getString("Password",null);
+        int user_id= prefs.getInt("Id",0);
+
+        //post para obtener los dominios
+        try {
+            httpClient = new DefaultHttpClient();
+            httpPost = new HttpPost("http://crisoldeideas.com/exporta/api_layer/purchaseReception.php");
+            nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("nick", usr));
+            nameValuePairs.add(new BasicNameValuePair("password", pass));
+            nameValuePairs.add(new BasicNameValuePair("purchase_employee_id", Integer.toString(user_id)));
+            nameValuePairs.add(new BasicNameValuePair("purchase_reception_date", get_fecha()));
+            nameValuePairs.add(new BasicNameValuePair("purchase_id", Integer.toString(purchase_id)));
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            responseHandler = new BasicResponseHandler();
+            response = httpClient.execute(httpPost, responseHandler);
+            aux= response.toString();
+
+            if (aux != null) {
+                Log.i("Registro de almacen", "Registro posteado\n ");
+            } else {
+                Log.e("Registro de almacen","fallo el Registro");
+            }
+            Log.e("Toda la respuesta",aux);
+            jsonObject = new JSONObject(aux);
+            Log.e("Response",jsonObject.toString());
+            JSONObject login_response = jsonObject.getJSONObject("response");
+            Log.e("Response Object", login_response.toString());
+
+
+            success=  login_response.getString("success");
+            status=  login_response.getString("status");
+
+        } catch (Exception ex) {
+            error = true;
+            authorization="error";
+            Log.e("error",ex.toString());
+            return authorization;
+        }
+        authorization="exito";
+        return authorization;
+    }
+
+    @TargetApi(Build.VERSION_CODES.CUPCAKE)
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        progressDialog.hide();
+        if (!error) {
+            Log.d("Registro de compra","Datos correctos");
+        } else {
+            Log.d("Registro de compra","Datos incorrectos");
+        }
+        progressDialog.dismiss();
+        if(success.equals("granted")){
+            Toast.makeText(context, " Compra Registrada ", Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            Toast.makeText(context,"Error al registrar la compra",Toast.LENGTH_SHORT).show();
+        }
+    }
+    private String get_fecha(){
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//dd/MM/yyyy
+        Date now = new Date();
+        String strDate = sdfDate.format(now);
+        Log.d("fecha",strDate);
+        return strDate;
+    }
+}
--- app/src/main/java/com/empresa/oscar/exportando/Purchase.java
+++ app/src/main/java/com/empresa/oscar/exportando/Purchase.java
@@ -1,14 +1,27 @@
 package com.empresa.oscar.exportando;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by UsuarioRasa on 25/10/2014.
@@ -48,8 +61,10 @@ class ViewHolderPurchase {
     TextView producto_texto;
     TextView cantidad_texto;
     TextView fecha_texto;
+    Button boton_compra;
 }
-class purchaseAdapter extends BaseAdapter {
+class purchaseAdapter extends BaseAdapter
+{
     protected Activity activity;
     protected ArrayList<Purchase> items;
 
@@ -74,7 +89,7 @@ class purchaseAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         // TODO Auto-generated method stub
         View vi=convertView;
         ViewHolderPurchase viewhold;
@@ -82,20 +97,78 @@ class purchaseAdapter extends BaseAdapter {
         if(vi == null|| !(vi.getTag() instanceof ViewHolderLocacion)) {
             LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             vi = inflater.inflate(R.layout.purchase_list_item, null);
+
             viewhold=new ViewHolderPurchase();
             viewhold.proveedor_texto=(TextView) vi.findViewById(R.id.proveedor);
             viewhold.producto_texto=(TextView) vi.findViewById(R.id.producto);
             viewhold.cantidad_texto=(TextView) vi.findViewById(R.id.cantidad);
             viewhold.fecha_texto=(TextView) vi.findViewById(R.id.fecha);
+            viewhold.boton_compra=(Button)vi.findViewById(R.id.recibido);
         }
         else{
             viewhold = (ViewHolderPurchase)vi.getTag();
         }
+
         Purchase pr = items.get(position);
         viewhold.proveedor_texto.setText(pr.getProveedor());
         viewhold.producto_texto.setText(pr.getProducto());
         viewhold.cantidad_texto.setText(Integer.toString(pr.getCantidad()));
         viewhold.fecha_texto.setText(pr.getFecha());
+        viewhold.boton_compra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Purchase actualpurchase=items.get(position);
+
+               new AlertDialog.Builder(activity)
+                        .setTitle(R.string.RecepcionCompra)
+                        .setMessage(R.string.ConfirmaRecepcionCompra)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                String exito="";
+                                // continue with delete
+                                try {
+                                   exito=new PostPurchaseReception(activity,actualpurchase.getId_compra()).execute().get();
+                                    if(exito.equals("exito")){
+                                        SharedPreferences prefs = activity.getSharedPreferences("Exporta",Activity.MODE_PRIVATE);
+                                        String nick=prefs.getString("Empleado",null);
+                                        String pass=prefs.getString("Password",null);
+                                        String type=prefs.getString("Type",null);
+                                        Intent compra_producto = new Intent(activity, PurchaseActivity.class);
+                                        compra_producto.putExtra("nick",nick);
+                                        compra_producto.putExtra("pass",pass);
+                                        compra_producto.putExtra("type",type);
+                                        compra_producto.putExtra("process","compra");
+                                        compra_producto.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        compra_producto.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                        activity.finish();
+                                        activity.startActivity(compra_producto);
+
+                                    }
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                } catch (ExecutionException e) {
+                                    e.printStackTrace();
+                                }
+
+                                Log.e("Registrando","compra "+Integer.toString(actualpurchase.getId_compra()));
+
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                                Log.e("Cancela", "compra " + Integer.toString(actualpurchase.getId_compra()));
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+
+            }
+        });
+
         return(vi);
     }
+
 }
+
